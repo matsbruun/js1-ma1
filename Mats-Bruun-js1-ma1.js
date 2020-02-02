df18e17b8a1b077a6477acb1dd27d1bcf0251b5c
matsbruun <55954689+matsbruun@users.noreply.github.com>
2020-02-02 22:40:16 +0100

    Add files via upload

---
+++ Mats-Bruun-js1-ma1.js
@@ -0,0 +1,32 @@
+// question 1
+const cat {
+  complain: function() {
+    console.log("Meow!");
+  }
+}
+
+// question 2
+const heading = document.querySelector("h3");
+console.dir(heading);
+
+// question 3
+heading.style.fontSize = "2em";
+
+// question 4
+const element = document.querySelector("heading");
+
+element.classList.add("subheading");
+
+// question 5
+const paragraphs = document.querySelectorAll("p");
+
+// question 6
+const resultsContainer = document.querySelector(".results");
+container.innerHTML = `<p>
+New paragraph
+</p>`;
+
+// question 7
+// question 8
+// question 9
+// question 10
